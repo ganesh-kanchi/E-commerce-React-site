eb1c00edd455ec15bfe03f97b3a255bd7d3a0130
Ganesh Kanchi <ganeshkanchimad@gmail.com>
2022-04-05 20:29:12 +0530

    minor changes to home page

--- src/components/Horizontal Card/Horizontal-Card.jsx
+++ src/components/Horizontal Card/Horizontal-Card.jsx
@@ -1,14 +1,14 @@
 import "./Horizontal-Card.css"
 
 export const HorizontalCard = ({product})=>{
-    const { _id, name, price, image } = product;
+    const { _id, name, price, image ,categoryName } = product;
     return (
     <div class="card-wrapper basic-card card-horizontal featured-card">
         <div class="row">
             <img class="card-img" src={image} alt="Perfume"/>
             <div class="card-main">
                 <div class="card-txt-badge primary">Trending</div>
-                <p class="heading">{name}</p>
+                <p class="heading">{name} for ({categoryName})</p>
                 <div class="card-content">
                     <div class="price">₹{price}</div>
                 </div>
