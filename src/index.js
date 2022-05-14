e24fe2c5f679e0ff3faab0033531b9f58324a19a
Ganesh Kanchi <86827594+ganesh-kanchi@users.noreply.github.com>
2022-05-14 16:34:17 +0530

    implemented suggested changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
-// import ReactDOM from "react-dom";
 import { createRoot } from 'react-dom/client';
 import App from "./App";
 import { makeServer } from "./server";
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { ProductsProvider } from "./contexts/productsContext";
 
 
@@ -19,14 +18,3 @@ root.render(
       </ProductsProvider>
     </Router>
   </React.StrictMode>)
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Router>
-//       <ProductsProvider>
-//         <App/>
-//       </ProductsProvider>
-//     </Router>
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
